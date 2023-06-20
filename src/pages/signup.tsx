6d91ca6615e3fb887e0adc2842d014172b623615
apinanyogaratnam <apinan@ontopical.com>
2023-06-19 22:30:17 -0400

    feat: signup page

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -4,7 +4,9 @@ const Signup = () => {
     return (
         <div>
             <h1>Signup</h1>
-            <Link href="/login" aria-label="login">Already have an account? Login</Link>
+            <Link href="/login" aria-label="login">
+                Already have an account? Login
+            </Link>
         </div>
     );
 };
