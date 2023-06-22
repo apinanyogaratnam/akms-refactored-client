9d8d3ab85b6896a1b657f4361128126f49184d0b
apinanyogaratnam <apinan@ontopical.com>
2023-06-21 23:40:42 -0400

    feat: setup for toast and react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import "@/styles/globals.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
+import { ToastContainer } from "react-toastify";
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Component {...pageProps} />
+            <ToastContainer autoClose={1500} closeOnClick={true} />
+        </QueryClientProvider>
+    );
 }
