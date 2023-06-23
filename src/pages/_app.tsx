6abad2829d02fd1133f2a8b3514948dfa3ee8c2e
apinanyogaratnam <apinan@ontopical.com>
2023-06-22 21:47:04 -0400

    feat: enabled vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Analytics } from "@vercel/analytics/react";
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
@@ -15,8 +16,9 @@ const MyApp: AppType<{ session: Session | null }> = ({ Component, pageProps: { s
         <SessionProvider session={session}>
             <QueryClientProvider client={queryClient}>
                 <Component {...pageProps} />
+                <ToastContainer autoClose={1500} closeOnClick={true} />
+                <Analytics />
             </QueryClientProvider>
-            <ToastContainer autoClose={1500} closeOnClick={true} />
         </SessionProvider>
     );
 };
