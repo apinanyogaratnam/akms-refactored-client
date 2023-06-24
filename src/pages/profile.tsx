88572463ed8c695ceee4c0b972742c4a778a7381
apinanyogaratnam <apinan@ontopical.com>
2023-06-23 20:50:15 -0400

    feat: displaying message when no api keys are created

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -69,33 +69,43 @@ const Profile = () => {
                 </div>
             ) : (
                 <>
-                    {(APIKeys || []).map((APIKey) => (
-                        <div key={APIKey.id} className="mx-auto w-full p-2 md:w-[60%]">
-                            <div className="border-bg-slate-200 flex flex-row items-center justify-around rounded-md border-2 p-2 md:justify-between">
-                                <div>
-                                    <p className="min-w-[250px] max-w-[250px] text-sm md:text-xl">
-                                        {APIKey.name.length > 20 ? APIKey.name.slice(0, 20) + "..." : APIKey.name}
-                                    </p>
-                                    <p className="md:text-md text-sm">{APIKey.description}</p>
+                    {APIKeys.length !== 0 ? (
+                        <>
+                            {APIKeys.map((APIKey) => (
+                                <div key={APIKey.id} className="mx-auto w-full p-2 md:w-[60%]">
+                                    <div className="border-bg-slate-200 flex flex-row items-center justify-around rounded-md border-2 p-2 md:justify-between">
+                                        <div>
+                                            <p className="min-w-[250px] max-w-[250px] text-sm md:text-xl">
+                                                {APIKey.name.length > 20
+                                                    ? APIKey.name.slice(0, 20) + "..."
+                                                    : APIKey.name}
+                                            </p>
+                                            <p className="md:text-md text-sm">{APIKey.description}</p>
+                                        </div>
+                                        <div>
+                                            <p>******************</p>
+                                            <p className="md:text-md text-sm">{APIKey.created_at}</p>
+                                        </div>
+                                        <div>
+                                            <button
+                                                className="border-bg-black rounded-md border-2 bg-slate-100 px-2 py-1 text-red-500 hover:bg-slate-200"
+                                                onClick={() => {
+                                                    setSelectedAPIKey(APIKey);
+                                                    setDeleteAPIKeyDialogOpened(true);
+                                                }}
+                                            >
+                                                Delete
+                                            </button>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div>
-                                    <p>******************</p>
-                                    <p className="md:text-md text-sm">{APIKey.created_at}</p>
-                                </div>
-                                <div>
-                                    <button
-                                        className="border-bg-black rounded-md border-2 bg-slate-100 px-2 py-1 text-red-500 hover:bg-slate-200"
-                                        onClick={() => {
-                                            setSelectedAPIKey(APIKey);
-                                            setDeleteAPIKeyDialogOpened(true);
-                                        }}
-                                    >
-                                        Delete
-                                    </button>
-                                </div>
-                            </div>
+                            ))}
+                        </>
+                    ) : (
+                        <div className="flex flex-row justify-center mt-10">
+                            <p className="text-xl">API Keys will appear here</p>
                         </div>
-                    ))}
+                    )}
                 </>
             )}
         </div>
